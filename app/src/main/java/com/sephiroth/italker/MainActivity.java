2192ac20ed2f4a41b4d340e8ad923878e25d0d3d
zhuyi <http://admin@zhuyi_pc:10101/r/Test.git>
2019-03-19 15:34:47 +0800

    主界面布局搭建

--- app/src/main/java/com/sephiroth/italker/MainActivity.java
+++ app/src/main/java/com/sephiroth/italker/MainActivity.java
@@ -1,31 +1,68 @@
 package com.sephiroth.italker;
 
-import android.annotation.SuppressLint;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.sephiroth.common.app.BaseActivity;
+import com.sephiroth.common.widget.PortraitView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
-    @BindView(R.id.tv_test)
-    TextView mTextView;
+    @BindView(R.id.appbar)
+    AppBarLayout mAppBarLayout;
+
+    @BindView(R.id.portrait)
+    PortraitView mPortraitView;
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;
+
+    @BindView(R.id.lay_containar)
+    FrameLayout mContainar;
+
+    @BindView(R.id.bottom_navigation_view)
+    BottomNavigationView mNavigationView;
 
     @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     protected void initWidget() {
         super.initWidget();
-        mTextView.setText("butterKnife");
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop()
+                .into(new ViewTarget<View, GlideDrawable>(mAppBarLayout) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        view.setBackground(resource.getCurrent());
+                    }
+                });
     }
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    protected void initData() {
-        super.initData();
+    @OnClick(R.id.im_search)
+    protected void onSearch() {
+
+    }
+
+    @OnClick(R.id.float_action_button)
+    protected void onAction() {
+
+    }
+
+    @OnClick(R.id.portrait)
+    protected void onPortraitClick() {
+
     }
 }
---
+++ common/src/main/java/com/sephiroth/common/widget/PortraitView.java
@@ -0,0 +1,24 @@
+package com.sephiroth.common.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * @author Sephiroth on 2019/3/19.
+ */
+
+public class PortraitView extends CircleImageView {
+    public PortraitView(Context context) {
+        super(context);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PortraitView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
